4f9a506000658daa59097847a3ac739e226083c8
rkdwl <gkang0702@gmail.com>
2025-08-08 17:41:32 +0900

    Initial commit

---
+++ OIIA_OIIA_lang.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Basic data types, 1 character
+#define O int
+#define i char
+#define I double
+#define A void
+
+// Operators, 2 ~ 3 characters
+#define oi +
+#define ii -
+#define ia * 
+#define aa /
+#define OO %
+#define II == 
+#define IA !=
+#define AA !
+#define Oo <
+#define Ii >
+#define Ia <=
+#define Aa >=
+#define oii &&
+#define iia ||
+#define OII ++
+#define IIA --
+
+// Keywords (Control, Loop, basic functions, etc.), 4 ~ characters
